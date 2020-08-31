3d1b43d6275774a5129066b9c10cb64456d9a783
Tiana Husted <tiana.husted@gmail.com>
2020-08-31 01:07:42 -0700

    Adding init() function

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,4 +1,21 @@
+const sql = require("mysql");
 const inquirer = require("inquirer");
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employeeTracker_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the init function after the connection is made to prompt the user
+  init();
+});
 
 const init = () => {
   inquirer.prompt([
@@ -8,7 +25,28 @@ const init = () => {
       message: "What would you like to do?",
       choices: ["Add a department", "Add a role", "Add an employee", "View all departments", "View all roles", "View all employees", "Update an employee role"]
     }
-  ])
+  ]).then((answers) => {
+    if(answers.mainMenu === "Add a department") {
+      addDepartment();
+      console.log("You want to add a department");
+    } else if(answers.mainMenu === "Add a role") {
+      addRole();
+      console.log("You want to add a role");
+    } else if(answers.mainMenu === "Add an employee") {
+      addEmployee();
+      console.log("You want to add an employee");
+    } else if(answers.mainMenu === "View all departments") {
+      viewAllDepartments();
+      console.log("You want to view all departments");
+    } else if(answers.mainMenu === "View all roles") {
+      viewAllRoles();
+      console.log("You want to view all roles");
+    } else if(answers.mainMenu === "View all employees") {
+      viewAllEmployees();
+      console.log("You want to view all employees");
+    } else {
+      updateEmployeeRole();
+      console.log("You want to update an employee role");
+    }
+  });
 }
-
-init();
